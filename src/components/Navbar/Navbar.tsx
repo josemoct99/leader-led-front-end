395a38890b8702aa7fdd369a1377f1b87673abe8
Henry Ricaurte Mora <hricaurtem@proton.me>
2024-12-10 09:14:22 -0500

    Modify navbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,16 +1,19 @@
 import "./Navbar.css";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 export const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <div className="navbar-logo">Leader Led S.A.S.</div>
-      <ul className="navbar-links">
-        <li><Link to="/">Información</Link></li>
-        <li><Link to="/services">Servicios</Link></li>
-        <li><Link to="/products">Productos</Link></li>
-        <li><Link to="/contact">Contáctenos</Link></li>
-      </ul>
-    </nav>
-  );
+    return (
+        <nav className="navbar">
+            <div className="navbar-logo">
+                <img src="/images/logos/leaderled.svg" alt="Leader Led Logo" style={{height: '80px', width: '220px'}}/>
+            </div>
+            <ul className="navbar-links">
+                <li><Link to="/">Inicio</Link></li>
+                <li><Link to="/services">Servicios</Link></li>
+                <li><Link to="/products">Productos</Link></li>
+                <li><Link to="/contact">Contáctenos</Link></li>
+                <li><Link to="/contact">Carrito</Link></li> {/* here maybe we should put an icon of shop idk*/}
+            </ul>
+        </nav>
+    );
 };
