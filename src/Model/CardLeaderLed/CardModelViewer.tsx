60ad09981af27882dc8a9b5b0643db6c74634b1b
Henry Ricaurte Mora <hricaurtem@proton.me>
2024-12-13 09:50:38 -0500

    improved 3d model

--- src/Model/CardLeaderLed/CardModelViewer.tsx
+++ src/Model/CardLeaderLed/CardModelViewer.tsx
@@ -1,21 +1,33 @@
 import {Canvas} from "@react-three/fiber";
-import {OrbitControls,useGLTF} from "@react-three/drei";
+import './CardModelViewer.css'
+import {OrbitControls, useGLTF} from "@react-three/drei";
 
 const Card = () => {
-    const {scene} = useGLTF("./model/cardLeaderLed.glb")
+    const {scene} = useGLTF("./model/cardLeaderLed.glb");
+
     /* eslint-disable-next-line react/no-unknown-property */
-    return <primitive object={scene}></primitive>
+    return <primitive object={scene}>
+
+    </primitive>
 }
 
 
 export const CardModelViewer = () => {
+
     return (
-        <Canvas style={{width:"100%", height:"500px" }}>
+        <Canvas className="canvas-model-viewer" style={{width: "40em", height: "30em"}}>
             <ambientLight/>
             {/* eslint-disable-next-line react/no-unknown-property */}
-            <pointLight position={[0, 10, 10]}></pointLight>
+            <pointLight position={[10, 10, 10]}></pointLight>
             <Card/>
-            <OrbitControls/>
+            <OrbitControls
+                autoRotate autoRotateSpeed={1.5}
+                enableZoom={true}
+                maxPolarAngle={Math.PI / 2}
+                minPolarAngle={Math.PI / 4}
+                maxDistance={15}
+                minDistance={5}
+            />
         </Canvas>
     )
 }
