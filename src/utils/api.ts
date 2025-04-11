6b131a997466369be13ddf2497f19d4bb50faef2
Jose Ricaurte <jose.ricaurte99@gmail.com>
2025-04-10 20:54:23 -0500

    Add environment variable for API URL and update .env.example

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,4 @@
-export const BASE_API_URL = "https://leaderledbackend.onrender.com/api/";
+export const BASE_API_URL = process.env['REACT_APP_BASE_API_URL'];
 export const INVENTORY_SEARCH_URL = `${BASE_API_URL}inventory/search`;
 export const BRAND_URL = `${BASE_API_URL}brand/`;
 
